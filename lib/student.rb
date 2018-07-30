92b0de35a1f4261ebf267700f4267f2f85c4da91
JohnMark <johnmarkludwick@gmail.com>
2018-07-30 17:07:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,17 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  attr_accessor :name, :grade
+  
+  def initialize
+    @name = name
+    @grade = grade
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  
+  
 end
